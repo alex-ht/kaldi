72901993315d88a0fc7f8f1006b32d5bc8d16067
alex-ht <AlexHT_Hung@asus.com>
2020-08-10 23:01:23 +0800

    Mish

--- egs/wsj/s5/steps/libs/nnet3/xconfig/basic_layers.py
+++ egs/wsj/s5/steps/libs/nnet3/xconfig/basic_layers.py
@@ -883,6 +883,13 @@ class XconfigBasicLayer(XconfigLayerBase):
                         ''.format(self.name, nonlinearity, output_dim,
                                   self_repair_scale))
 
+            elif nonlinearity == 'softplus':
+                line = ('component name={0}.{1}'
+                        ' type=SoftplusComponent dim={2}'
+                        ' self-repair-scale={3}'
+                        ''.format(self.name, nonlinearity, output_dim,
+                                  self_repair_scale))
+
             elif nonlinearity == 'renorm':
                 add_log_stddev = "false"
                 if i == len(nonlinearities) - 1:
